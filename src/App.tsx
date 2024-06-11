cfaed6dd5e439695db31b5701e7b3070de4ff67b
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-06-11 10:53:42 +0200

    CouchSurfing
    CouchSurfing

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+import Logo from "../src/assets/yellow-sofa.png";
+
+function App() {
+  return (
+    <>
+      <h1 className="p-4 text-red-600 text-4xl">App Couchsurfing</h1>
+      <img src={Logo} alt="" />
+    </>
+  );
+}
+
+export default App;
